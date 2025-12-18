9f6846ec5031bd25131895d0e2f1d7daac0b4987
Michael <20736150+uimc@users.noreply.github.com>
2025-12-18 15:53:29 -0600

    Add files via upload
    table adjustments

--- js/commas.js
+++ js/commas.js
@@ -6,5 +6,5 @@ function displayLocalStorageWithCommas() {
     document.getElementById('transfers_count').textContent = addComma(localStorage.getItem("tr_co"));
     document.getElementById('holders_count').textContent = addComma(localStorage.getItem("ho_co"));
     document.getElementById('circulating_supply').textContent = addComma(localStorage.getItem("ci_su"));
-    document.getElementById('transfered_amount').textContent = addComma(localStorage.getItem("tr_am"));
+    //document.getElementById('transfered_amount').textContent = addComma(localStorage.getItem("tr_am"));
 }
